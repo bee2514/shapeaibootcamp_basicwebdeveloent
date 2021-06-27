605e2a82133ee5b3691b8429dcac9427582802cc
Ben Basil Paul <84306065+bee2514@users.noreply.github.com>
2021-06-27 20:13:27 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function info(){
+  return(
+    <div className="note">
+  <h1>Javascript and React.js</h1>
+  <p>A basic web developent React Js Bootcamp</p>
+  </div>
+  );
+  }
+  export default info;
+
